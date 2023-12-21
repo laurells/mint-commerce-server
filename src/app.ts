3b7996d2997a4e103b1d6895ac5083d7f03c2de2
Philip-D21 <philiplekan88@gmail.com>
2023-12-21 14:40:50 +0100

    folder structure for auth-service

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import express from "express";
+//import morgan from "morgan";
+//import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+
+const app = express();
+
+// Enable CORS
+app.use(cors());
+app.use(bodyParser.json()); // Parse JSON bodies
+// Set HTTP Hseaders
+//app.use(helmet());
+// app.use(morgan('dev'));
+
+// Set Rate Limit
+const limiter = rateLimit({
+  windowMs: 7 * 60 * 1000, // 7 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
+
+// process.env.NODE_ENV === "development" && app.use(morgan("dev"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+export default app;
+function morgan(dev: any): any {
+  throw new Error("Function not implemented.");
+}
+
